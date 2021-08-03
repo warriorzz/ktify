8948f1f98056ae8b7f67390f752871293e009835
warrior <warriormayer@gmail.com>
2021-08-03 19:09:55 +0200

    Move Cursor.kt

--- src/main/kotlin/com/github/warriorzz/ktify/model/player/RecentPlayback.kt
+++ src/main/kotlin/com/github/warriorzz/ktify/model/player/RecentPlayback.kt
@@ -2,7 +2,7 @@ package com.github.warriorzz.ktify.model.player
 
 import com.github.warriorzz.ktify.model.Track
 import com.github.warriorzz.ktify.model.util.Context
-import com.github.warriorzz.ktify.model.utils.Cursor
+import com.github.warriorzz.ktify.model.util.Cursor
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
---
+++ src/main/kotlin/com/github/warriorzz/ktify/model/util/Cursor.kt
@@ -0,0 +1,17 @@
+package com.github.warriorzz.ktify.model.util
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Cursor(
+    val after: String
+)
+
+@Serializable
+data class CursorPagingObject(
+    val cursors: Cursor,
+    val href: String,
+    val limit: Int,
+    val next: String? = null,
+    val total: Int,
+)
